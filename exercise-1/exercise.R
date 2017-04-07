98363da9bf03e33b94375b48d9cefe0ceda971cf
Siddarth nalegave <Siddarth nalegave>
2017-04-06 17:40:30 -0700

    made changes

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,16 @@
 
 # Create a vector of the number of points the Seahawks scored the first 4 games of last season
 # Hint: (google "Seahawks scores 2016")
+points.for <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+point.against <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
-
+all.points <- data.frame(points.for, point.against)
 
 # Create a new column "diff" that is the difference in points
-
+all.points$diff <- all.points$points.for - all.points$point.against
 
 # Create a new column "won" which is TRUE if the Seahawks wom
 
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,25 +1,25 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- paste("employee", 1:100)
 
 # Create a vector of 2014 salaries using the runif function
-
+salaries.2014 <- runif(100, 400, 600)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.2015 <- runif(100, 450, 650)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries.2015 - salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+print(salaries[47, 'salaries.2015'])
 
 # How many employees got a raise?
 
